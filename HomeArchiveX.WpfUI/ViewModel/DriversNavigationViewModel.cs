70226a68944339b0a00347a383122ca3012c1d7e
elkrot <elkrot@gmail.com>
2017-04-24 13:10:17 +0300

    Add ViewModel Empty classes

---
+++ HomeArchiveX.WpfUI/ViewModel/DriversNavigationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeArchiveX.WpfUI.ViewModel
+{
+
+    public interface IDriversNavigationViewModel
+    {
+        void Load();
+    }
+
+    public class DriversNavigationViewModel : IDriversNavigationViewModel
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeArchiveX.WpfUI/ViewModel/DriversViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeArchiveX.WpfUI.ViewModel
+{
+   public class DriversViewModel : Observable
+    {
+    }
+}
