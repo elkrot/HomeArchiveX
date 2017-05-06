ef9724f551a20cef7b8d7535bf99a5b921ad289b
elkrot <elkrot@gmail.com>
2017-05-06 13:51:18 +0300

    work

--- HomeArchiveX.WpfUI/View/Files/FilesOnDriveNavigationView.xaml.cs
+++ HomeArchiveX.WpfUI/View/Files/FilesOnDriveNavigationView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,61 @@ namespace HomeArchiveX.WpfUI.View
     /// </summary>
     public partial class FilesOnDriveNavigationView : UserControl
     {
+        ObservableCollection<Node> nodes;
         public FilesOnDriveNavigationView()
         {
             InitializeComponent();
+            nodes = new ObservableCollection<Node>
+        {
+            new Node
+            {
+                Name ="Европа",
+                Nodes = new ObservableCollection<Node>
+                {
+                    new Node {Name="Германия" },
+                    new Node {Name="Франция" },
+                    new Node
+                    {
+                        Name ="Великобритания",
+                        Nodes = new ObservableCollection<Node>
+                        {
+                            new Node {Name="Англия" },
+                            new Node {Name="Шотландия" },
+                            new Node {Name="Уэльс" },
+                            new Node {Name="Сев. Ирландия" },
+                        }
+                    }
+                }
+            },
+            new Node
+            {
+                Name ="Азия",
+                Nodes = new ObservableCollection<Node>
+                {
+                    new Node {Name="Китай" },
+                    new Node {Name="Япония" },
+                    new Node { Name ="Индия" }
+                }
+            },
+            new Node { Name="Африка" },
+            new Node { Name="Америка" },
+            new Node { Name="Австралия" }
+        };
+            filesTreeView.ItemsSource = nodes;
+        }
+
+        public class Node
+        {
+            public string Name { get; set; }
+            public ObservableCollection<Node> Nodes { get; set; }
+        }
+
+        private void filesTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            //  http://www.wpf-tutorial.com/treeview-control/treeview-data-binding-multiple-templates/
+            // http://www.wpf-tutorial.com/treeview-control/handling-selection-expansion-state/
+            // https://blogs.msdn.microsoft.com/mikehillberg/2009/10/30/treeview-and-hierarchicaldatatemplate-step-by-step/
+            //  System.Windows.Forms.MessageBox.Show(((Node)e.OriginalSource).Name);
         }
     }
 }
