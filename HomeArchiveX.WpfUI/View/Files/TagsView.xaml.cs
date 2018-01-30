11f8f63cf60a2f4b4bdcdb1f442b0442e2acabc6
Kolodiazhnyi Serhii <elkrot@gmail.com>
2018-01-30 13:34:38 +0200

    work

--- HomeArchiveX.WpfUI/View/Files/TagsView.xaml.cs
+++ HomeArchiveX.WpfUI/View/Files/TagsView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HomeArchiveX.WpfUI.View.Files
 {
@@ -20,6 +9,20 @@ namespace HomeArchiveX.WpfUI.View.Files
     /// </summary>
     public partial class TagsView : UserControl
     {
+
+        #region RemoveItem
+        public static readonly DependencyProperty RemoveItemProperty =
+            DependencyProperty.Register(
+                "RemoveItem",
+                typeof(ICommand),
+                typeof(TagsView),
+                new UIPropertyMetadata(null));
+        public ICommand RemoveItem
+        {
+            get { return (ICommand)GetValue(RemoveItemProperty); }
+            set { SetValue(RemoveItemProperty, value); }
+        }
+        #endregion
         public TagsView()
         {
             InitializeComponent();
